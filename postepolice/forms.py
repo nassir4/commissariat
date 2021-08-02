d8624f412a17d2a9c56875003d6ede6c91a59c8b
nassir4 <mamadounassir.diallo@uadb.edu.sn>
2021-08-02 20:32:09 +0000

    garde a vue section poste de pollice

--- postepolice/forms.py
+++ postepolice/forms.py
@@ -1,7 +1,7 @@
 from bootstrap_datepicker_plus import TimePickerInput
 from django.forms import ModelForm, TimeField
 
-from postepolice.models import MainCourante, Plainte, Perte, ObjectConsigne, Ecrou, Registre
+from postepolice.models import MainCourante, Plainte, Perte, ObjectConsigne, Ecrou, Registre, GardeAVue
 
 
 class MainCouranteForm(ModelForm):
@@ -14,7 +14,7 @@ class MainCouranteForm(ModelForm):
 class PlainteForm(ModelForm):
     class Meta:
         model = Plainte
-        fields = ['numero_mention','heure','motif']
+        fields = ['numero_mention','heure','motif','photo1','photo2']
     heure = TimeField(
             widget=TimePickerInput()
         )
@@ -35,7 +35,7 @@ class ObjectConsigneForm(ModelForm):
 class EcrouForm(ModelForm):
     class Meta:
         model =Ecrou
-        fields = ['numero_mention','heure','petite_identite','objects_consignes']
+        fields = ['numero_mention','heure','petite_identite','objects_consignes','motif_garde_vue','suite']
     heure = TimeField(
             widget=TimePickerInput()
         )
@@ -43,3 +43,7 @@ class RegistreForm(ModelForm):
     class Meta:
         model = Registre
         fields = ['brigade']
+class GardeAVueForm(ModelForm):
+    class Meta:
+        model = GardeAVue
+        fields = ['numero','nom','prenom','date_naissance','lieu_naissance','profession','domicile']
--- postepolice/models.py
+++ postepolice/models.py
@@ -1,3 +1,6 @@
+from random import choices
+
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 
 # Create your models here.
@@ -7,6 +10,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 from django_summernote.fields import SummernoteTextField
 
+from accident.views import modifierDeclaration
+
 ROLES = (
     ('Chef de poste', '0'),
     ('Secretaire', '1'),
@@ -14,6 +19,7 @@ ROLES = (
     ('Police de secours', '3')
 )
 
+fs = FileSystemStorage(location='/media/photos')
 
 # modele agent poste
 class AgentPoste(models.Model):
@@ -61,6 +67,8 @@ class Plainte(models.Model):
     numero_mention = models.IntegerField("Numero mention", null=True, blank=True)
     heure = models.TimeField("Heure", auto_now_add=False)
     motif = SummernoteTextField("Contenu")
+    photo1 = models.ImageField(storage=fs)
+    photo2= models.ImageField(storage=fs)
     # signature
     status = models.BooleanField("Status", default=False)
 
@@ -93,8 +101,44 @@ class Ecrou(models.Model):
     heure = models.TimeField("Heure", auto_now_add=False)
     petite_identite = SummernoteTextField("Petite identité")
     objects_consignes = models.CharField("Objet consigné",max_length=400, blank=True,null=True)
+    motif_garde_vue = models.CharField("Motif garde à vue",max_length=255)
+    suite = models.CharField("Suite",max_length=255)
     status = models.BooleanField("Status", default=False)
 
     def __str__(self):
         return self.numero_mention
 
+class GardeAVue(models.Model):
+    numero = models.IntegerField("N°", null=True, blank=True)
+    registre = models.ForeignKey(Registre, on_delete=models.CASCADE,blank=True,null=True)
+    nom = models.CharField("Nom", max_length=255,blank=True,null=True)
+    prenom = models.CharField("Prénom",max_length=255,blank=True,null=True)
+    date_naissance = models.DateField("Date de naissance", auto_now=False,blank=True,null=True)
+    lieu_naissance = models.CharField("Lieu de naissance", max_length=255,blank=True,null=True)
+    profession = models.CharField("Profession", max_length=255,blank=True,null=True)
+    domicile = models.CharField("Domicile",max_length=255,blank=True,null=True)
+    motif = SummernoteTextField("Motif de la garde à vue",blank=True,null=True)
+    prise_par = models.CharField("Prise par", max_length=255,blank=True,null=True)
+    date_debut = models.DateField("Date de debut",auto_now=False,blank=True,null=True)
+    heure_debut = models.TimeField("Heure",auto_now=False,blank=True,null=True)
+    heure_debut_audition = models.TimeField("Heure de debut audition",auto_now=False,blank=True,null=True)
+    heure_fin_audition = models.TimeField("heure de fin audition", auto_now=False,blank=True,null=True)
+    duree_audition = models.CharField("Durée audition",max_length=255,blank=True,null=True)
+    heure_debut_repos = models.TimeField("Heure de debut repos", auto_now=False,blank=True,null=True)
+    heure_fin_repos = models.TimeField("heure de fin repos", auto_now=False,blank=True,null=True)
+    duree_repos = models.CharField("Durée repos", max_length=255,blank=True,null=True)
+    liberte = models.CharField("Liberté", max_length=255,blank=True,null=True)
+    date_conduite = models.DateField("Date conduite", auto_now=False,blank=True,null=True)
+    heure_conduite = models.TimeField("Heure conduite", auto_now=False,blank=True,null=True)
+    devant = models.CharField("Devant", max_length=255,blank=True,null=True)
+    numero_pv = models.CharField("P.V.n°", max_length=255,blank=True,null=True)
+    date_pv = models.DateField("Date PV", auto_now=False,blank=True,null=True)
+    date_prolongation = models.DateField("Date de la prolongation de la garde à vue", auto_now=False,blank=True,null=True)
+    heure_prolongation = models.TimeField("Heure de la prolongation de la garde à vue", auto_now=False,blank=True,null=True)
+    aupres_de = models.CharField("Auprès de", max_length=255,blank=True,null=True)
+    Decision_choice=(
+        ('AC','Accorée'),
+        ('RE','Refusé')
+    )
+    decision_magistrat = models.CharField("Décision du magistrat", default='AC', choices=Decision_choice, max_length=255,blank=True,null=True)
+    observations = models.CharField("Observations", max_length=255,blank=True,null=True)
