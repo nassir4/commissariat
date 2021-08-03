c0d3978240df0231f9cd5451318f7c68ef76a93f
nassir4 <mamadounassir.diallo@uadb.edu.sn>
2021-08-03 20:33:12 +0000

    ecrou

--- commissariat/settings.py
+++ commissariat/settings.py
@@ -152,8 +152,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 INTERNAL_IPS = ['127.0.0.1']
 X_FRAME_OPTIONS = 'SAMEORIGIN'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- postepolice/admin.py
+++ postepolice/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from postepolice.models import AgentPoste, Brigade, MainCourante, Registre, Ecrou
+from postepolice.models import AgentPoste, Brigade, MainCourante, Registre, Ecrou, Plainte
 
 admin.site.register(AgentPoste)
 admin.site.register(Brigade)
 admin.site.register(MainCourante)
 admin.site.register(Registre)
 admin.site.register(Ecrou)
+admin.site.register(Plainte)
