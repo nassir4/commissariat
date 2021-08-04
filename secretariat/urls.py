8ef9f7444630f876812e785d466067b70320c901
nassir4 <mamadounassir.diallo@uadb.edu.sn>
2021-08-04 16:08:30 +0000

    poste de police  dans secretariat

--- secretariat/urls.py
+++ secretariat/urls.py
@@ -7,5 +7,27 @@ app_name='secretariat'
 urlpatterns = [
     path('accueil/', views.index,name='index'),
     path('chartJSON', ChartData.as_view(), name='line_chart_json'),
-    path('chartJSSON2',ChartPie.as_view(),name='bar_chart_json')
+    path('chartJSSON2',ChartPie.as_view(),name='bar_chart_json'),
+
+    path('plainte/', views.plainte, name='plainte'),
+    path('plainte/detail/<int:id>/', views.detailPlainte, name='detail_plainte'),
+    path('perte/', views.perte, name='perte'),
+    path('perte/detail/<int:id>/', views.detailPerte, name='detail_perte'),
+    path('ecrou/', views.ecrou, name='ecrou'),
+    path('ecrou/detail/<int:id>/', views.detailEcrou, name='detail_ecrou'),
+    path('main_courante/', views.mainCourante, name='main_courante'),
+    path('main_courante/detail/<int:id>/', views.detailMainCourante, name='detail_main_courante'),
+    path('garde_a_vue/detail/<int:id>/', views.detailGardeAVue, name='detail_garde'),
+    path('main_courante/liste_registre/',views.listRegistreMC, name='liste_registre_MC'),
+    path('ecrou/liste_registre/',views.listRegistreEc, name='liste_registre_Ec'),
+    path('plainte/liste_registre/',views.listRegistrePl, name='liste_registre_Pl'),
+    path('perte/liste_registre/',views.listRegistrePer, name='liste_registre_Per'),
+    path('garde_a_vue/liste_registre/', views.listRegistreGarde, name='liste_registre_Garde'),
+
+    path('main_courante/registre/detail/<int:id>/', views.detailRegistreMC, name='detail_registre_MC'),
+    path('plainte/registre/detail/<int:id>/', views.detailRegistrePl, name='detail_registre_Pl'),
+    path('perte/registre/detail/<int:id>/', views.detailRegistrePer, name='detail_registre_Per'),
+    path('ecrou/registre/detail/<int:id>/', views.detailRegistreEc, name='detail_registre_Ec'),
+    path('garde_a_vue/registre/detail/<int:id>/', views.detailRegistreGarde, name='detail_registre_Garde'),
+
 ]
--- secretariat/views.py
+++ secretariat/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from rest_framework.response import Response
@@ -9,7 +10,9 @@ from rest_framework.views import APIView
 from accident.models import Accident
 from authentification.decorators import allowed_user
 from pojudiciaire.models import Crime
-from postepolice.models import Registre
+from postepolice.forms import RegistreForm
+from postepolice.models import Registre, Plainte, Perte, Ecrou, MainCourante, GardeAVue
+
 
 @login_required(login_url='login:secretariat')
 @allowed_user(allowed_roles=['secretariat'])
@@ -70,3 +73,179 @@ class ChartPie(APIView):
             "default":default_items
         }
         return Response(data)
+
+
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def plainte(request):
+    listPlainte = Plainte.objects.all
+    context = {
+        'listPlainte': listPlainte
+    }
+    return render(request, 'poste/plainte/plainte.html',context);
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def detailPlainte(request, id):
+    try:
+        plainte = Plainte.objects.get(pk=id)
+    except Plainte.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'poste/plainte/detail_plainte.html', {'plainte': plainte})
+
+
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def perte(request):
+    listPerte = Perte.objects.all
+    context = {
+        'listPerte': listPerte
+    }
+    return render(request, 'poste/perte/perte.html',context);
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def detailPerte(request, id):
+    try:
+        pv = Perte.objects.get(pk=id)
+    except Perte.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'poste/perte/detail_perte.html', {'perte': perte})
+
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def ecrou(request):
+    listEcrou = Ecrou.objects.all
+    context = {
+        'listEcrou': listEcrou
+    }
+    return render(request, 'poste/ecrou/ecrou.html',context);
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def detailEcrou(request,id):
+    try:
+        ecrou=Ecrou.objects.get(pk=id)
+    except Ecrou.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'poste/ecrou/detail_ecrou.html', {'ecrou':ecrou})
+
+
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def mainCourante(request):
+    listMainCourante = MainCourante.objects.all
+    context = {
+        'listMainCourante': listMainCourante
+    }
+    return render(request, 'poste/courante/courante.html',context);
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def detailMainCourante(request, id):
+    try:
+        mainCourante =MainCourante.objects.get(pk=id)
+    except MainCourante.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'poste/courante/detail_courante.html', {'mainCourante': mainCourante})
+
+
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def listRegistreMC(request):
+    listRegistre = Registre.objects.filter(nom="Main Courante")
+    context = {
+        'listRegistre' : listRegistre,
+    }
+    return render(request, 'poste/courante/courante.html',context)
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def listRegistrePl(request):
+    listRegistre = Registre.objects.filter(nom="Plainte")
+    context = {
+        'listRegistre' : listRegistre,
+    }
+    return render(request, 'poste/plainte/plainte.html',context)
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def listRegistreEc(request):
+    listRegistre = Registre.objects.filter(nom="Ecrou")
+    context = {
+        'listRegistre' : listRegistre,
+    }
+    return render(request, 'poste/ecrou/ecrou.html',context)
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def listRegistrePer(request):
+    listRegistre = Registre.objects.filter(nom="Perte")
+    context = {
+        'listRegistre' : listRegistre,
+    }
+    return render(request, 'poste/perte/perte.html',context)
+
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def listRegistreGarde(request):
+    listRegistre = Registre.objects.filter(nom="Garde à vue")
+    context = {
+        'listRegistre' : listRegistre,
+    }
+    return render(request, 'poste/garde/garde_a_vue.html',context)
+
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def detailRegistreMC(request,id):
+    registre = Registre.objects.get(pk=id)
+    listMainCourante = MainCourante.objects.filter(registre=registre)
+    context = {
+        'registre':registre,
+        'listMainCourante':listMainCourante
+    }
+    return render(request, 'poste/courante/detail_courante.html',context)
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def detailRegistreGarde(request,id):
+    registre = Registre.objects.get(pk=id)
+    listGarde = GardeAVue.objects.filter(registre=registre)
+    context = {
+        'registre':registre,
+        'listGarde':listGarde
+    }
+    return render(request, 'poste/garde/detail_registre.html',context)
+
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def detailRegistrePl(request,id):
+    registre = Registre.objects.get(pk=id)
+    listPlainte = Plainte.objects.filter(registre=registre)
+    context = {
+        'registre':registre,
+        'listPlainte':listPlainte
+    }
+    return render(request, 'poste/plainte/detail_plainte.html',context)
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def detailRegistrePer(request,id):
+    registre = Registre.objects.get(pk=id)
+    listPerte = Perte.objects.filter(registre=registre)
+    context = {
+        'registre':registre,
+        'listPerte':listPerte
+    }
+    return render(request, 'poste/perte/detail_perte.html',context)
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def detailRegistreEc(request,id):
+    registre = Registre.objects.get(pk=id)
+    listEcrou = Ecrou.objects.filter(registre=registre)
+    context = {
+        'registre':registre,
+        'listEcrou':listEcrou
+    }
+    return render(request, 'poste/ecrou/detail_ecrou.html',context)
+
+
+@login_required(login_url='login:secretariat')
+@allowed_user(allowed_roles=['secretariat'])
+def detailGardeAVue(request, id):
+    try:
+        garde =GardeAVue.objects.get(pk=id)
+    except GardeAVue.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'poste/garde/detail_garde.html', {'garde': garde})
